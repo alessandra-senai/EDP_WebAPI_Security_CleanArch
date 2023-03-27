cb695931a79c1536cdc75f740974ad0e5cd158c1
Alessandra Soares dos Santos <alessandra.ss@edu.sc.senai.br>
2023-03-27 12:23:18 -0300

    Adicionar arquivos de projeto.

---
+++ WebAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,68 @@
+﻿using Application.DTOs;
+using Application.Service;
+using AutoMapper;
+using Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers
+{
+    [Route("api/employee")]
+    [ApiController]
+    //[Authorize]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeesController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet]
+        [Authorize(Roles = ("Employee, Manager, Administrator"))]
+        public async Task<ActionResult<dynamic>> GetEmployee()
+        {
+            return await _employeeService.GetEmployee();
+        }
+
+        [HttpGet("{id}")]
+        [Authorize(Roles = ("Employee, Manager, Administrator"))]
+        public async Task<ActionResult<dynamic>> GetEmployee(int id)
+        {
+           return await _employeeService.GetEmployee(id);
+        }
+
+        [HttpPut("update-salary/{id}")]
+        [Authorize(Roles = ("Manager"))]
+        public async Task<ActionResult<dynamic>> PutEmployee(int id, Employee employee)
+        {
+            return await _employeeService.PutEmployee(id, employee);    
+        }
+
+        [HttpPost("create-employee")]
+        [Authorize(Roles = ("Administrator"))]
+        public async Task<ActionResult<dynamic>> PostEmployee(FuncionarioRequest request)
+        {
+            return await _employeeService.PostEmployee(request);
+        }
+
+        [HttpDelete("delete-employee/{id}")]
+        [Authorize(Roles = ("Manager, Administrator"))]
+        public async Task<ActionResult<dynamic>> DeleteEmployee(int id)
+        {
+          return await _employeeService.DeleteEmployee(id);
+        }
+
+        [HttpDelete("delete-manager/{id}")]
+        [Authorize(Roles = ("Administrator"))]
+        public async Task<ActionResult<dynamic>> DeleteManager(int id)
+        {
+           return await _employeeService.DeleteManager(id);
+        }        
+    }
+}
